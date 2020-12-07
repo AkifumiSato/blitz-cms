2658607904d05526bb6d79c9917d9b9ec4943d3f
佐藤昭文 <akfm.sato@gmail.com>
2020-12-08 00:03:55 +0900

    delete tags logic

--- app/blogs/mutations/deleteBlog.ts
+++ app/blogs/mutations/deleteBlog.ts
@@ -6,6 +6,8 @@ type DeleteBlogInput = Pick<BlogDeleteArgs, 'where'>
 export default async function deleteBlog({ where }: DeleteBlogInput, ctx: Ctx) {
   ctx.session.authorize()
 
+  // TODO: remove once Prisma supports cascading deletes
+  await db.tag.deleteMany({ where: { blog: { id: where.id } } })
   const blog = await db.blog.delete({ where })
 
   return blog
